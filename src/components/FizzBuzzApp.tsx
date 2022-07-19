ed5c8bd572aac0fb6f7f6cb9ba72472d08118527
syed <user.email>
2022-07-19 18:18:39 +0100

    added simple counter

--- src/components/FizzBuzzApp.tsx
+++ src/components/FizzBuzzApp.tsx
@@ -1,14 +1,40 @@
 import { useState } from "react";
 
 function FizzBuzz(): JSX.Element {
-  const [counterValuesFromCurrentRender, queueRerenderWithNewCounterValue] =
-    useState<string[]>(["0"]);
+
+  const [counterValuesFromCurrentRender, queueRerenderWithNewCounterValues] =
+    useState(0);
+//   const [storedValuesFromCurrentRender, queueRerenderWithNewStoredValues] =
+//     useState<string[]>([]);
+
+/*
+    [val,updateVal] = useState(0)
+    fizzArray = [0]
+
+    buttonClick() {
+        val++
+
+        IF val MOD 3 = 0 AND val mod 5 = 0 {
+            APPEND fizzArray WITH 'fizzbuzz'
+            UPDATE updateVal WITH 'fizzbuzz'
+        }
+        Else If (val MOD 3 = 0) {
+            APPEND fizzArray WITH 'fizz'
+        }
+    }
+*/
+  const handleNextValue = () => {
+    queueRerenderWithNewCounterValues(counterValuesFromCurrentRender + 1);
+  };
+
+
+
 
   return (
     <>
       <h1>Fizz Buzz</h1>
-      <p>counter: </p>
-      <button>NEXT</button>
+      <p>counter: {counterValuesFromCurrentRender}</p>
+      <button onClick={handleNextValue}>NEXT</button>
     </>
   );
 }
