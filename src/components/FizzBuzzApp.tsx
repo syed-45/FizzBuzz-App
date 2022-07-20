9c634a7933d3953ea45d8acaf9e4647625645b29
syed <user.email>
2022-07-20 09:30:45 +0100

    added fizzbuzz conditions and changed start to 1

--- src/components/FizzBuzzApp.tsx
+++ src/components/FizzBuzzApp.tsx
@@ -3,17 +3,36 @@ import { useState } from "react";
 function FizzBuzz(): JSX.Element {
 
   const [counterValueFromCurrentRender, queueRerenderWithNewCounterValue] =
-    useState(0);
+    useState(1);
   const [storedValuesFromCurrentRender, queueRerenderWithNewStoredValues] =
     useState<string[]>([]);
 
 
   const handleNextValue = () => {
     queueRerenderWithNewCounterValue(counterValueFromCurrentRender + 1);
-    queueRerenderWithNewStoredValues([
-        ...storedValuesFromCurrentRender,
-        counterValueFromCurrentRender.toString(),
-      ]);
+    
+    if (counterValueFromCurrentRender%5===0 && counterValueFromCurrentRender%3===0) {
+        queueRerenderWithNewStoredValues([
+            ...storedValuesFromCurrentRender,
+            'FizzBuzz ',
+          ]);
+    } else if (counterValueFromCurrentRender%3===0) {
+        queueRerenderWithNewStoredValues([
+            ...storedValuesFromCurrentRender,
+            'Fizz ',
+          ]);
+    } else if (counterValueFromCurrentRender%5===0) {
+        queueRerenderWithNewStoredValues([
+            ...storedValuesFromCurrentRender,
+            'Buzz ',
+          ]);
+    } else {
+        queueRerenderWithNewStoredValues([
+            ...storedValuesFromCurrentRender,
+            counterValueFromCurrentRender.toString()+' ',
+          ]);
+    }
+
   };
 
 
